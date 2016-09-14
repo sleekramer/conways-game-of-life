bed64a9676cd633e17d113493832f18aa62e0659
Ryan Schenck <Ryan@Ryans-iMac-184.local>
2016-09-13 23:34:06 -0400

    Update 11.34

--- rs_conway.py
+++ rs_conway.py
@@ -35,20 +35,42 @@ def blanks():
 				gridDict[x,y] = cell((x,y), 1)
 	return gridDict
 
-def check_neighbors(indcell):
-	# passes x,y of the cell looked up from blanks and returns 0 or 1
-	
+def check_neighbors(item):
+	alive = 0
+	dead = 0
+	newStat = 0
 
-	return indcell
+	# passes x,y of the cell looked up from blanks and returns 0 or 1
+	for x in [10, -10, 0]:
+		for y in [10, -10, 0]:
+			if x != 0 and y != 0:
+				alive += gridDict[(item[0]+x,item[1]+y)].stat
+			else:
+				dead += gridDict[(item[0]+x,item[1]+y)].stat
+
+	if dead == 1:
+		if alive < 2 or alive > 3:
+			newStat = 0
+		else:
+			newStat = 1
+	elif dead == 0:
+		if alive == 3:
+			newStat = 1
+		else:
+			pass
+
+	return cell(item, newStat)
 
 # cycles through
 def tick():
 	newDraw = {}
 	for item in gridDict:
-		newDraw[item] = check_neighbors(gridDict[item])
+		newDraw[item] = check_neighbors(item)
+	gridDict = newDraw
+	color()
 
 def color():
-	to_col = blanks()
+	to_col = gridDict
 	for item in to_col:
 		if to_col[item].stat == 1:
 			pygame.draw.rect(display_surface, white, (to_col[item].loc[0],to_col[item].loc[1],cellsize,cellsize))
@@ -62,10 +84,17 @@ def main():
 	display_surface.fill(black)
 	pygame.display.set_caption('Game of Life') 
 
+	blanks()
 	color()
+	# for item in gridDict:
+	# 	print(item)
+	# 	print(gridDict[item])
+	# 	sys.exit()
+
 	drawGrid()
 	while True: #main game loop
 		for event in pygame.event.get():
+			print(gridDict[0,0])
 			tick()
 			if event.type == QUIT:
 				pygame.quit()
