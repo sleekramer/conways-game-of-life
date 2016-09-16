f001c86929d45a358012bbd32d447322ed760a42
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2016-09-16 12:54:08 -0400

    Ready for app development

--- functionality.py
+++ functionality.py
@@ -10,6 +10,82 @@ b_red = (255,0,0)
 green = (0,200,0)
 b_green = (0,255,0)
 
+def render_textrect(string, font, rect, text_color, background_color, justification=0):
+    """Returns a surface containing the passed text string, reformatted
+    to fit within the given rect, word-wrapping as necessary. The text
+    will be anti-aliased.
+
+    Takes the following arguments:
+
+    string - the text you wish to render. \n begins a new line.
+    font - a Font object
+    rect - a rectstyle giving the size of the surface requested.
+    text_color - a three-byte tuple of the rgb value of the
+                 text color. ex (0, 0, 0) = BLACK
+    background_color - a three-byte tuple of the rgb value of the surface.
+    justification - 0 (default) left-justified
+                    1 horizontally centered
+                    2 right-justified
+
+    Returns the following values:
+
+    Success - a surface object with the text rendered onto it.
+    Failure - raises a TextRectException if the text won't fit onto the surface.
+    """
+
+    import pygame
+    
+    final_lines = []
+
+    requested_lines = string.splitlines()
+
+    # Create a series of lines that will fit on the provided
+    # rectangle.
+
+    for requested_line in requested_lines:
+        if font.size(requested_line)[0] > rect.width:
+            words = requested_line.split(' ')
+            # if any of our words are too long to fit, return.
+            for word in words:
+                if font.size(word)[0] >= rect.width:
+                    raise TextRectException, "The word " + word + " is too long to fit in the rect passed."
+            # Start a new line
+            accumulated_line = ""
+            for word in words:
+                test_line = accumulated_line + word + " "
+                # Build the line while the words fit.    
+                if font.size(test_line)[0] < rect.width:
+                    accumulated_line = test_line 
+                else: 
+                    final_lines.append(accumulated_line) 
+                    accumulated_line = word + " " 
+            final_lines.append(accumulated_line)
+        else: 
+            final_lines.append(requested_line) 
+
+    # Let's try to write the text out on the surface.
+
+    surface = pygame.Surface(rect.size) 
+    surface.fill(background_color) 
+
+    accumulated_height = 0 
+    for line in final_lines: 
+        if accumulated_height + font.size(line)[1] >= rect.height:
+            raise TextRectException, "Once word-wrapped, the text string was too tall to fit in the rect."
+        if line != "":
+            tempsurface = font.render(line, 1, text_color)
+            if justification == 0:
+                surface.blit(tempsurface, (0, accumulated_height))
+            elif justification == 1:
+                surface.blit(tempsurface, ((rect.width - tempsurface.get_width()) / 2, accumulated_height))
+            elif justification == 2:
+                surface.blit(tempsurface, (rect.width - tempsurface.get_width(), accumulated_height))
+            else:
+                raise TextRectException, "Invalid justification argument: " + str(justification)
+        accumulated_height += font.size(line)[1]
+
+    return surface
+
 def round_up_nearest_ten(n):
 	return int(math.ceil(n / 10.0)) * 10
 
@@ -33,7 +109,7 @@ def user_select(gridDict, x, y):
 		if gridDict[x,y].stat == 1:
 			gridDict[x,y].stat = 0
 		else:
-			print 'gridDict[%s,%s].stat = 1' % (x,y)
+			# print 'gridDict[%s,%s].stat = 1' % (x,y)
 			gridDict[x,y].stat = 1
 
 def text_objects(text, font):
@@ -105,6 +181,68 @@ def reset(display_surface, x, y, run, pos=(0,0), mstate=(0,0,0)):
 	display_surface.blit(textSurf, textRect)
 
 	return run, res_menu
+
+def info_button(display_surface, x, y, run, pos=(0,0), mstate=(0,0,0)):
+	xanchor = int(x/2.)
+	yanchor = int(y/2.)
+
+	pygame.draw.circle(display_surface, black, ((x-x+20), (y-25+(20/2))), 10, 2)
+
+	smallText = pygame.font.Font("freesansbold.ttf",15)
+	textSurf, textRect = text_objects("i", smallText)
+	textRect.center = ( (x-x+20), (y-25+(20/2)+2))
+	display_surface.blit(textSurf, textRect)
+
+	# print str((pos[0],pos[1]))
+	if x-x+30 > pos[0] > x-x+10 and y-5 > pos[1] > y-22:
+		if mstate[0]:
+			show_i = True
+			run = False
+		else:
+			show_i = False
+	else:
+		show_i = False
+
+	return run, show_i
+
+def info(display_surface, x, y, pos=(0,0), mstate=(0,0,0)):
+	xanchor = int(x/2.)
+	yanchor = int(y/2.)
+	pygame.draw.rect(display_surface, white, (0, 0, x, y))
+
+	smallText = pygame.font.Font("freesansbold.ttf",18)
+	textSurf, textRect = text_objects("Exit", smallText)
+	textRect.center = ( x-x+20 , y-y+10 )
+	display_surface.blit(textSurf, textRect)
+
+	text_info = """John Conway's Cellular Automaton"""
+	text_info2 = """\n\nRules:\n\nEach Cell with one or no neighbors dies.\nEach cell with 4 or more neighbors dies.\nEach cell with two or three neighbors survives.\nEach empty cell with three neighbors becomes populated.\n\nControls:\n\nClick to populate/depopulate a cell.\nClick start to begin model.\nClick stop to end model.\nClick reset to clear or choose preset simulations.\n\nCreated by:\n\nRyan Oliver Schenck & Steven Lee-Kramer\n\ngithub.com/rschenck/game_of_life.git"""
+	
+	# Headers
+	smallText = pygame.font.Font(None,24)
+	back_rect = pygame.Rect((x-x, y-y+60, x, y-50))
+	rendered_text = render_textrect(text_info, smallText, back_rect, black, white, 1)
+	display_surface.blit(rendered_text, back_rect)
+
+	# Other Text
+	smallText = pygame.font.Font(None,18)
+	back_rect = pygame.Rect((x-x, y-y+80, x, y-50))
+	rendered_text = render_textrect(text_info2, smallText, back_rect, black, white, 1)
+	display_surface.blit(rendered_text, back_rect)
+
+
+	# print str((pos[0],pos[1]))
+	if x-x+40 > pos[0] > x-x and y-y+18 > pos[1] > y-y:
+		if mstate[0]:
+			show_i = False
+		else:
+			show_i = True
+	else:
+		show_i = True
+
+
+	return show_i
+
 
 def reset_options(display_surface, x, y, pos=(0,0), mstate=(0,0,0)):
 	xanchor = int(x/2.)
