f6d656ec3168dd19d6650434a8d0f405bad66b7a
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2016-09-16 15:07:54 -0400

    Test .gitignore

--- functionality.py
+++ functionality.py
@@ -286,9 +286,9 @@ def reset_options(display_surface, x, y, pos=(0,0), mstate=(0,0,0)):
 	textRect.center = ( (xanchor), (0-(y/4.0)+450) )
 	display_surface.blit(textSurf, textRect)
 
-	# print str((pos[0],pos[1]))
+	print str((pos[0],pos[1]))
 
-	s = 60
+	s = 30
 	# clear
 	if xanchor+30 > pos[0] > xanchor-30 and s+5 > pos[1] > s-10:
 		if mstate[0]:
